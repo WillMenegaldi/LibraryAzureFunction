4f03182936697c8d2ca07fc37183829d0a9473c6
William Menegaldi <willmenegaldi@gmail.com>
2018-11-19 11:36:45 -0200

    Library Get method working with Azure Function HTTP

--- LibraryFunction/BooksFunction.cs
+++ LibraryFunction/BooksFunction.cs
@@ -4,30 +4,48 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using LibraryDataAgent.Interfaces;
+using LibraryDataAgent.Models;
+using LibraryDataAgent;
+using AzureFunctions.Autofac;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace LibraryFunction
 {
+    [DependencyInjectionConfig(typeof(DIConfig))]
     public static class BooksFunction
     {
         [FunctionName("GetMethod")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            [Inject]IBooksDataAgent bookDataAgent,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string book = req.Query["idbook"];
+
+            string select = $"SELECT * FROM BOOK WHERE ISBN = {book}";
+            string selectAll = "SELECT * FROM BOOK";
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            book = book ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            List<string[]> livro = new List<string[]>();
+
+            if (book != null)
+            {
+                livro = bookDataAgent.Select(select);
+            } else
+            {
+                livro = bookDataAgent.Select(selectAll);
+            }
+
+            return new OkObjectResult(livro);
         }
     }
 }
