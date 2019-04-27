f6024a3e0e49b97258fd580c5c3259776fdd1492
William Menegaldi <44971939+WillMenegaldi@users.noreply.github.com>
2019-04-27 10:42:39 -0300

    Removing Book class constructor

--- LibraryDataAgent.cs/Models/Books.cs
+++ LibraryDataAgent.cs/Models/Books.cs
@@ -10,13 +10,5 @@ namespace LibraryDataAgent.Models
         public string Nmbook { get; set; }
         public int Idauthor { get; set; }
         public int Idpublisher { get; set; }
-
-        public Books(string code, string name, int author, int publisher)
-        {
-            Isbn = code;
-            Nmbook = name;
-            Idauthor = author;
-            Idpublisher = publisher;
-        }
     }
 }
